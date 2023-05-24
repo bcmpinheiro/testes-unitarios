c6cb33affceff3c3866d6aec308124c947b91eb3
Barbara Pinheiro <bcmpinheiro@gmail.com>
2023-05-23 23:04:05 -0300

    review assert

--- src/main/java/com/bcmp/entity/Usuario.java
+++ src/main/java/com/bcmp/entity/Usuario.java
@@ -17,4 +17,21 @@ public class Usuario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (this == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+		}
+	}
---
+++ src/test/java/com/bcmp/service/AssertTest.java
@@ -0,0 +1,44 @@
+package com.bcmp.service;
+
+import com.bcmp.entity.Usuario;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertTest {
+
+    @Test
+    public void teste() {
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+
+        Assert.assertEquals("Erro de Comparacao",1,1);
+        Assert.assertEquals(0.51,0.51, 0.01);
+        Assert.assertEquals(Math.PI, 3.14,0.01);
+
+        int i = 5;
+        Integer i2 = 5;
+        Assert.assertEquals(Integer.valueOf(i), i2);
+        Assert.assertEquals(i, i2.intValue());
+
+        Assert.assertEquals("bola", "bola");
+        Assert.assertNotEquals("bola", "casa");
+        Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+        Assert.assertTrue("bola".startsWith("bo"));
+
+        Usuario u1 = new Usuario("Usuario 1");
+        Usuario u2 = new Usuario("Usuario 1");
+        Usuario u3 = null;
+
+        Assert.assertEquals(u1, u2);
+
+        Assert.assertSame(u2, u2);
+        Assert.assertNotSame(u1, u2);
+
+        Assert.assertNull(u3);
+        Assert.assertNotNull(u2);
+
+
+
+
+    }
+}
